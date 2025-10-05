08f5a6ac1c3163b71ba70398444e63face6cc0b1
edikleprikon <160333543+edikleprikon@users.noreply.github.com>
2025-10-05 13:36:04 +0300

    tonconnect-manifest.js

---
+++ tonconnect-manifest.js
@@ -0,0 +1,7 @@
+{
+    "url": "https://your-app.com",
+    "name": "Game App",
+    "iconUrl": "https://your-app.com/icon.png",
+    "termsOfUseUrl": "https://your-app.com/terms",
+    "privacyPolicyUrl": "https://your-app.com/privacy"
+}
